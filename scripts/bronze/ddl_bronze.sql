8709ddecefee4d99db08cfe2afe27aee210ea9e7
raramutiara <32611506+raramutiara@users.noreply.github.com>
2025-09-10 11:27:08 +0700

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -51,7 +51,7 @@ IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_loc_a101;
 CREATE TABLE bronze.erp_loc_a101 (
 	cid NVARCHAR (50),
-	cntry NVARCHAR (50),
+	cntry NVARCHAR (50)
 );
 
 IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
